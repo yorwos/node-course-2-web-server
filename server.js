c08a888349db740991a37ca6782898a2532c860a
yorgos <yorgos78@hotmail.com>
2017-06-22 21:10:07 +0100

    Projects page added

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/about', (req, res) => {
   })
 })
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio Page',
+  })
+})
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
